7cad825369cd89f65733ee13ec2621d74ff93805
Herman Adu <herman.adu@hotmail.com>
2022-03-12 17:37:03 +0000

    Created the postWidget component implemented recent post on home page

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,21 +1,11 @@
 /* eslint-disable quotes */
 import React from "react";
-import Image from "next/image";
+/* import Image from "next/image"; */
 import moment from "moment";
 import Link from "next/link";
 
 const PostCard = ({ post }) => (
   <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-    {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
-      <Image
-        unoptimized
-        loader={grpahCMSImageLoader}
-        alt={post.title}
-        className="shadow-lg rounded-t-lg lg:rounded-lg"
-        layout="fill"
-        src={post.featuredImage.url}
-      />
-    </div> */}
     <div className="relative overflow-hidden shadow-md pb-80 mb-6">
       <img
         src={post.featuredImage.url}
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,10 +1,61 @@
+/* eslint-disable no-console */
 /* eslint-disable quotes */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import moment from "moment";
+import Link from "next/link";
 
-const PostWidget = () => (
-  <div>
-    <h2>PostWidget</h2>
-  </div>
-);
+import { getSimilarPosts, getRecentPosts } from "../services";
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
+    }
+  }, [slug]);
+
+  console.log("Related Post:", relatedPosts);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              className="text-md"
+              key={post.title}
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default PostWidget;
